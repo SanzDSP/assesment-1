e80e15843a79584dbbed9f783d6b7355c3044d57
SanzDSP <137406888+SanzDSP@users.noreply.github.com>
2023-10-13 21:55:25 +0700

    Update list_data.dart

--- puntens/lib/ui/list_data.dart
+++ puntens/lib/ui/list_data.dart
@@ -1,10 +1,10 @@
+// Sandi S
 import 'package:flutter/material.dart';
 
 class PersonalDataDisplay extends StatelessWidget {
   final List<Map<String, String>>? data;
   final VoidCallback clearData;
 
-  // PersonalDataDisplay({this.data});
   PersonalDataDisplay({required this.data, required this.clearData});
 
   @override
@@ -13,72 +13,40 @@ class PersonalDataDisplay extends StatelessWidget {
       appBar: AppBar(
         title: Text('Data Personal'),
       ),
-      body: Column(
-        children: <Widget>[
-          Expanded(
-            child: ListView.builder(
-              itemCount: data?.length,
-              itemBuilder: (context, index) {
-                final item = data?[index];
-                return ListTile(
-                  title: Text('Nama: ${item?['Name']}'),
-                  subtitle: Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: [
-                      Text('Email: ${item?['Email']}'),
-                      Text('Tanggal Lahir: ${item?['BirthDate']}'),
-                    ],
-                  ),
-                );
-              },
+      body: ListView.builder(
+        itemCount: data?.length,
+        itemBuilder: (context, index) {
+          final item = data?[index];
+          return Container(
+            margin: EdgeInsets.all(
+                10.0), // Margin untuk menambahkan ruang di sekitar setiap item
+            padding: EdgeInsets.all(
+                10.0), // Padding untuk memberikan jarak dari tepi border
+            decoration: BoxDecoration(
+              border: Border.all(color: Colors.black), // Border hitam
             ),
-          ),
-          ElevatedButton(
-            onPressed: clearData,
-            child: Text('Hapus Data'),
-          ),
-        ],
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Text(
+                  'Nama: ${item?['Name']}',
+                  style: TextStyle(fontWeight: FontWeight.bold), // Teks tebal
+                ),
+                Text(
+                  'Email: ${item?['Email']}',
+                  style: TextStyle(color: Colors.blue), // Warna teks biru
+                ),
+                Text('Address: ${item?['Address']}'),
+                // Text('Tanggal Lahir: ${item?['BirthDate']}'),
+              ],
+            ),
+          );
+        },
+      ),
+      bottomNavigationBar: ElevatedButton(
+        onPressed: clearData,
+        child: Text('Hapus Data'),
       ),
     );
   }
 }
-
-// class PersonalDataDisplay extends StatelessWidget {
-//   final String? name;
-//   final String? email;
-//   final String? phone;
-//   final String? id;
-//   final String? address;
-//   final String? birthDate;
-
-//   PersonalDataDisplay(
-//       {this.name,
-//       this.email,
-//       this.phone,
-//       this.id,
-//       this.address,
-//       this.birthDate});
-
-//   @override
-//   Widget build(BuildContext context) {
-//     return Scaffold(
-//       appBar: AppBar(
-//         title: Text('Data Personal'),
-//       ),
-//       body: SingleChildScrollView(
-//         child: Padding(
-//           padding: EdgeInsets.all(16.0),
-//           child: Column(
-//             crossAxisAlignment: CrossAxisAlignment.start,
-//             children: <Widget>[
-//               Text('Nama: $name'),
-//               Text('Email: $email'),
-//               Text('Email: $address'),
-//               // Text('Tanggal Lahir: $birthDate'),
-//             ],
-//           ),
-//         ),
-//       ),
-//     );
-//   }
-// }
